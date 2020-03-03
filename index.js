8166b4aa8e61a03914bcc068d9dbf67a107f076a
tihangrai <tihangrai@gmail.com>
2020-03-03 00:57:35 -0500

    refactors

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
 const express = require("express");
 const request = require("request");
 const cheerio = require("cheerio");
+const app = express();
 
 // Import helpers
 const strip_trailing_slash = require("./helpers/strip_trailing_slash");
 
-// Import recipies
-const corque_madam = require("./recipies/corque_madam");
-const dired_shredded_pollock = require("./recipies/dried_shredded_pollock");
-const cookies_and_cream = require("./recipies/cookies_and_cream");
-const panzella_with_mozzarella = require("./recipies/panzella_with_mozzarella");
-
-const app = express();
+// Import recipe helper
+const recipe_url_to_object = require("./recipes");
 
 // GET @recipe
 // Route - Recieves link to site and sends scraped instance
 app.get("/recipe", (req, res) => {
+  // Sanitize he URL- Helper method is stripping the trailing slash
   const url = strip_trailing_slash(req.query.url);
 
   request(url, (error, response, body) => {
@@ -26,62 +25,18 @@ app.get("/recipe", (req, res) => {
     // Load the page into cheerio instance
     const $ = cheerio.load(body);
 
-    // Declaring values that we want to extract from page
-    let name = "";
-    let ingredients;
-    let steps = [];
-    let recipeObj;
-
-    // Switch accoding to the URL provided
-    switch (url) {
-      case corque_madam.url:
-        name = corque_madam.getName($);
-        ingredients = corque_madam.getIngredients($);
-        steps = corque_madam.getSteps($);
-        recipeObj = Object.assign(
-          {},
-          { url: corque_madam.url, name, ingredients, steps }
-        );
-        break;
-
-      case dired_shredded_pollock.url:
-        name = dired_shredded_pollock.getName($);
-        ingredients = dired_shredded_pollock.getIngredients($);
-        steps = dired_shredded_pollock.getSteps($);
-        recipeObj = Object.assign(
-          {},
-          { url: dired_shredded_pollock.url, name, ingredients, steps }
-        );
-        break;
-
-      case cookies_and_cream.url:
-        name = cookies_and_cream.getName($);
-        ingredients = cookies_and_cream.getIngredients($);
-        steps = cookies_and_cream.getSteps($);
-        recipeObj = Object.assign(
-          {},
-          { url: cookies_and_cream.url, name, ingredients, steps }
-        );
-        break;
-
-      case panzella_with_mozzarella.url:
-        name = panzella_with_mozzarella.getName($);
-        ingredients = panzella_with_mozzarella.getIngredients($);
-        steps = panzella_with_mozzarella.getSteps($);
-        recipeObj = Object.assign(
-          {},
-          { url: panzella_with_mozzarella.url, name, ingredients, steps }
-        );
-        break;
-
-      default:
-        res.json({ message: "Oops! provide a valid URL" });
-        break;
-    }
-    res.send(recipeObj);
+    // Getting a recipe object after providing URL and cheerio instace to the method /recipes/index.js
+    recipe_object = recipe_url_to_object(url, $);
+    res.json(recipe_object);
   });
 });
 
-app.listen(8080, () => {
-  console.log(`Server started on port`, 8080);
+app.get("/", (req, res) => {
+  res.json({
+    message: "Go to GET /recipe and enter a valid URL"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port`, PORT);
 });
---
+++ recipes/index.js
@@ -0,0 +1,51 @@
+// This file is responsible to hold all scrapers
+// and switches according to given URL and cheerio instance
+
+// Import recipies
+const corque_madam = require("./corque_madam");
+const dired_shredded_pollock = require("./dried_shredded_pollock");
+const cookies_and_cream = require("./cookies_and_cream");
+const panzella_with_mozzarella = require("./panzella_with_mozzarella");
+
+// This function recieves a URL and Cheerio instance and returns a valid usable recipe object
+function recipe_url_to_object(url, $) {
+  // Pre-declaring values that we want to extract from page
+  let name = "";
+  let ingredients;
+  let steps = [];
+
+  // Switch accoding to the URL provided
+  switch (url) {
+    case corque_madam.url:
+      name = corque_madam.getName($);
+      ingredients = corque_madam.getIngredients($);
+      steps = corque_madam.getSteps($);
+      return Object.assign({}, { url, name, ingredients, steps });
+
+    case dired_shredded_pollock.url:
+      name = dired_shredded_pollock.getName($);
+      ingredients = dired_shredded_pollock.getIngredients($);
+      steps = dired_shredded_pollock.getSteps($);
+      return Object.assign({}, { url, name, ingredients, steps });
+
+    case cookies_and_cream.url:
+      name = cookies_and_cream.getName($);
+      ingredients = cookies_and_cream.getIngredients($);
+      steps = cookies_and_cream.getSteps($);
+      return Object.assign(
+        {},
+        { url: cookies_and_cream.url, name, ingredients, steps }
+      );
+
+    case panzella_with_mozzarella.url:
+      name = panzella_with_mozzarella.getName($);
+      ingredients = panzella_with_mozzarella.getIngredients($);
+      steps = panzella_with_mozzarella.getSteps($);
+      return Object.assign({}, { url, name, ingredients, steps });
+
+    default:
+      res.json({ message: "Oops! provide a valid URL" });
+  }
+}
+
+module.exports = recipe_url_to_object;
